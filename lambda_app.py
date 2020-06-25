fd1876ce207b1fd2c328437237156ba4dd2f8c90
Brian Antonelli <brian.antonelli@gmail.com>
2020-06-25 09:54:26 -0400

    smarthome working :thumbsup:

--- lambda_app.py
+++ lambda_app.py
@@ -1,96 +1,282 @@
+import logging
 import urllib.request
 import os
 import json
+import uuid
+import time
 
-# Class to define a home device that to be controlled by Alexa. eg. Lamp
-class AlexaHomeApp:
-    def __init__(self, applianceId, name, id):
-        self.endpointId = applianceId
-        self.manufacturerName = "weigao"
-        self.friendlyName = name
-        self.displayCategories = ["SWITCH"]
-        self.capabilities = [
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+base_url = os.environ["BASE_URL"]
+
+APPLIANCES = [
+    {
+        "applianceId": "smarttv-001",
+        "manufacturerName": "Antonelli Industries",
+        "modelName": "TVLift",
+        "version": "1",
+        "friendlyName": "Lift",
+        "friendlyDescription": "TV Lift which can be turned on (up) and off (down).",
+        "isReachable": True,
+        "actions": [
+            "turnOn",
+            "turnOff"
+        ],
+        "additionalApplianceDetails": {
+            "detail1": "foo",
+            "detail2": "bar"
+        }
+    },
+    {
+        "applianceId": "smarttv-002",
+        "manufacturerName": "Antonelli Industries",
+        "modelName": "TV",
+        "version": "1",
+        "friendlyName": "TV",
+        "friendlyDescription": "TV which can be turned on and off.",
+        "isReachable": True,
+        "actions": [
+            "turnOn",
+            "turnOff"
+        ],
+        "additionalApplianceDetails": {
+            "detail1": "foo",
+            "detail2": "bar"
+        }
+    }
+]
+
+def lambda_handler(request, context):
+    try:
+        logger.info("Directive:")
+        logger.info(json.dumps(request, indent=4, sort_keys=True))
+
+        version = get_directive_version(request)
+
+        if version == "3":
+            logger.info("Received v3 directive!")
+            if request["directive"]["header"]["name"] == "Discover":
+                response = handle_discovery_v3(request)
+            else:
+                response = handle_non_discovery_v3(request)
+
+        else:
+            logger.info("Received v2 directive!")
+            if request["header"]["namespace"] == "Alexa.ConnectedHome.Discovery":
+                response = handle_discovery()
+            else:
+                response = handle_non_discovery(request)
+
+        logger.info("Response:")
+        logger.info(json.dumps(response, indent=4, sort_keys=True))
+
+        return response
+    except ValueError as error:
+        logger.error(error)
+        raise
+
+# v2 handlers
+def handle_discovery():
+    header = {
+        "namespace": "Alexa.ConnectedHome.Discovery",
+        "name": "DiscoverAppliancesResponse",
+        "payloadVersion": "2",
+        "messageId": get_uuid()
+    }
+    payload = {
+        "discoveredAppliances": APPLIANCES
+    }
+    response = {
+        "header": header,
+        "payload": payload
+    }
+    return response
+
+def handle_non_discovery(request):
+    request_name = request["header"]["name"]
+
+    if request_name == "TurnOnRequest":
+        header = {
+            "namespace": "Alexa.ConnectedHome.Control",
+            "name": "TurnOnConfirmation",
+            "payloadVersion": "2",
+            "messageId": get_uuid()
+        }
+        payload = {}
+    elif request_name == "TurnOffRequest":
+        header = {
+            "namespace": "Alexa.ConnectedHome.Control",
+            "name": "TurnOffConfirmation",
+            "payloadVersion": "2",
+            "messageId": get_uuid()
+        }
+    # other handlers omitted in this example
+    payload = {}
+    response = {
+        "header": header,
+        "payload": payload
+    }
+    return response
+
+# v2 utility functions
+def get_appliance_by_appliance_id(appliance_id):
+    for appliance in APPLIANCES:
+        if appliance["applianceId"] == appliance_id:
+            return appliance
+    return None
+
+def get_utc_timestamp(seconds=None):
+    return time.strftime("%Y-%m-%dT%H:%M:%S.00Z", time.gmtime(seconds))
+
+def get_uuid():
+    return str(uuid.uuid4())
+
+# v3 handlers
+def handle_discovery_v3(request):
+    endpoints = []
+    for appliance in APPLIANCES:
+        endpoints.append(get_endpoint_from_v2_appliance(appliance))
+
+    response = {
+        "event": {
+            "header": {
+                "namespace": "Alexa.Discovery",
+                "name": "Discover.Response",
+                "payloadVersion": "3",
+                "messageId": get_uuid()
+            },
+            "payload": {
+                "endpoints": endpoints
+            }
+        }
+    }
+    return response
+
+def handle_non_discovery_v3(request):
+    request_namespace = request["directive"]["header"]["namespace"]
+    request_name = request["directive"]["header"]["name"]
+    endpoint_id = request["directive"]["endpoint"]["endpointId"]
+
+    if request_namespace == "Alexa.PowerController":
+        if request_name == "TurnOn":
+            value = "ON"
+        else:
+            value = "OFF"
+
+        if endpoint_id == "smarttv-002":
+            power = "on" if request_name == "TurnOn" else "off"
+            send_tv_request(power)
+        elif endpoint_id == "lift":
+            direction = "up" if request_name == "TurnOn" else "down"
+            send_lift_request(direction)
+
+
+        response = {
+            "context": {
+                "properties": [
+                    {
+                        "namespace": "Alexa.PowerController",
+                        "name": "powerState",
+                        "value": value,
+                        "timeOfSample": get_utc_timestamp(),
+                        "uncertaintyInMilliseconds": 500
+                    }
+                ]
+            },
+            "event": {
+                "header": {
+                    "namespace": "Alexa",
+                    "name": "Response",
+                    "payloadVersion": "3",
+                    "messageId": get_uuid(),
+                    "correlationToken": request["directive"]["header"]["correlationToken"]
+                },
+                "endpoint": {
+                    "scope": {
+                        "type": "BearerToken",
+                        "token": "access-token-from-Amazon"
+                    },
+                    "endpointId": request["directive"]["endpoint"]["endpointId"]
+                },
+                "payload": {}
+            }
+        }
+        return response
+
+    elif request_namespace == "Alexa.Authorization":
+        if request_name == "AcceptGrant":
+            response = {
+                "event": {
+                    "header": {
+                        "namespace": "Alexa.Authorization",
+                        "name": "AcceptGrant.Response",
+                        "payloadVersion": "3",
+                        "messageId": "5f8a426e-01e4-4cc9-8b79-65f8bd0fd8a4"
+                    },
+                    "payload": {}
+                }
+            }
+            return response
+
+# v3 utility functions
+def get_endpoint_from_v2_appliance(appliance):
+    endpoint = {
+        "endpointId": appliance["applianceId"],
+        "manufacturerName": appliance["manufacturerName"],
+        "friendlyName": appliance["friendlyName"],
+        "description": appliance["friendlyDescription"],
+        "displayCategories": ["SWITCH"],
+        "cookie": appliance["additionalApplianceDetails"],
+        "capabilities": [
+            {
+                "type": "AlexaInterface",
+                "interface": "Alexa.PowerController",
+                "version": "3",
+                "properties": {
+                    "supported": [
+                        { "name": "powerState" }
+                    ],
+                    "proactivelyReported": True,
+                    "retrievable": True
+                }
+            },
+            {
+                "type": "AlexaInterface",
+                "interface": "Alexa.EndpointHealth",
+                "version": "3",
+                "properties": {
+                    "supported":[
+                        { "name":"connectivity" }
+                    ],
+                    "proactivelyReported": True,
+                    "retrievable": True
+                }
+            },
             {
                 "type": "AlexaInterface",
                 "interface": "Alexa",
                 "version": "3"
-            },
-            {
-                "interface": "Alexa.PowerController",
-                "version": "3",
-                "type": "AlexaInterface",
-                "properties": {
-                    "supported": [{ "name": "powerState" }],
-                    "retrievable": "true"
-                }
             }
         ]
-
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__)
-
-def lambda_handler(event, context):
-    print(event)
-    eventname = event["directive"]["header"]["namespace"]
-    if eventname == "Alexa.Discovery":
-        return handleDiscovery(event["directive"]["header"])
-    elif eventname == "Alexa.PowerController":
-        return handleControl(event)
-
-
-base_url = os.environ["BASE_URL"]
-tv = AlexaHomeApp("tv", "TV", "tv")
-lift = AlexaHomeApp("lift", "Lift", "lift")
-
-# This function return all the devices in a JSON body.
-# see document in https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/smart-home-skill-api-reference#discovery-messages
-def handleDiscovery(header):
-    header["name"] = "Discover.Response"
-
-    payload = {"endpoints":
-        [
-            tv.__dict__,
-            lift.__dict__,
-        ]
     }
 
-    response = {"event":{
-        "header": header,
-        "payload": payload
-    }}
+    return endpoint
 
-    print(response)
-    return response
+def get_directive_version(request):
+    try:
+        return request["directive"]["header"]["payloadVersion"]
+    except:
+        try:
+            return request["header"]["payloadVersion"]
+        except:
+            return "-1"
 
-# This is the function to handle the event request. The event will be generated when you talk to Alexa Echo with a valid request.
-# See https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/smart-home-skill-api-reference#onoff-messages
-def handleControl(event):
-    name = "TurnOnConfirmation"
-
-    event_name = event["header"]["name"]
-    applianceId = event["payload"]["appliance"]["applianceId"]
-
-    if event_name == "TurnOnRequest":
-        name = "TurnOnConfirmation"
-    elif event_name == "TurnOffRequest":
-        name = "TurnOffConfirmation"
-
-    if applianceId == "tv":
-        power = "on" if event_name == "TurnOnRequest" else "off"
-        send_tv_request(power)
-    elif applianceId == "lift":
-        direction = "up" if event_name == "TurnOnRequest" else "down"
-        send_lift_request(direction)
-
-    header = {
-        "namespace": "Alexa.ConnectedHome.Control",
-        "name": name,
-        "payloadVersion": "2",
-    }
-    return {
-        "header": header,
-        "payload": {}
-    }
+def get_endpoint_by_endpoint_id(endpoint_id):
+    appliance = get_appliance_by_appliance_id(endpoint_id)
+    if appliance:
+        return get_endpoint_from_v2_appliance(appliance)
+    return None
 
 def send_tv_request(power):
     url = "{}/tv_power/{}".format(base_url, power)
