7de143e5c890fd91bc082377e75557e1dddbf498
Brian Antonelli <brian.antonelli@gmail.com>
2020-07-13 23:34:57 -0400

    rf changes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import logging
 import yaml
 import cec
+import RPi.GPIO as GPIO
+import atexit
 
 from flask import Flask
 from rpi_rf import RFDevice
@@ -8,23 +10,31 @@ from rpi_rf import RFDevice
 logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
                     format='%(asctime)-15s - [%(levelname)s] %(module)s: %(message)s',)
 
-with open('config.yml', 'r') as cf:
-    cfg = yaml.load_all(cf, Loader=yaml.FullLoader)
+with open('config.yml') as f:
+    cfg = yaml.load(f, Loader=yaml.FullLoader)
 
 app = Flask(__name__)
 
 # fixme: move into class
-rfdevice = RFDevice(cfg.lift.gpio)
+rfdevice = RFDevice(cfg['lift']['gpio'])
 rfdevice.enable_tx()
-rfdevice.tx_repeat = cfg.lift.repeat
+rfdevice.tx_repeat = cfg['lift']['repeat']
 
 # fixme: move into class
-cec.init()
-cec_device = cec.Device(0)
+# cec.init()
+# cec_device = cec.Device(0)
 
 def transmit_lift_code(code):
-    logging.info('lift rf transmit: %s', code)
-    rfdevice.tx_code(code, cfg.lift.protocol, cfg.lift.pulse, cfg.lift.length)
+    logging.info('lift rf transmit: %s; protocol: %s; pulse length: %d, length: %d', code, cfg['lift']['protocol'], cfg['lift']['pulse'], cfg['lift']['length'])
+    rfdevice.tx_code(code, cfg['lift']['protocol'], cfg['lift']['pulse'], cfg['lift']['length'])
+
+def at_shutdown():
+    logging.info('Shutting down, cleaning up RFDevice and GPIO..')
+    rfdevice.cleanup()
+    GPIO.cleanup()
+
+
+atexit.register(at_shutdown)
 
 @app.route('/')
 def index():
@@ -34,9 +44,9 @@ def index():
 def move_lift(direction='up'):
     logging.info('move lift: %s', direction)
     if direction == 'up':
-        transmit_lift_code(cfg.lift.code.up)
+        transmit_lift_code(cfg['lift']['code']['up'])
     else:
-        transmit_lift_code(cfg.lift.code.down)
+        transmit_lift_code(cfg['lift']['code']['down'])
 
     return 'ok'
 
@@ -55,5 +65,6 @@ def tv_power(power='on'):
     return 'ok'
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0:{}".format(cfg.app.port), debug=True)
+# if __name__ == "__main__":
+#     logging.info("StealthTV listening on 0.0.0.0:{}".format(cfg['app']['port']))
+#     app.run(host="127.0.0.1:{}".format(cfg['app']['port']), debug=True)
